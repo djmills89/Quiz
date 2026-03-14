e2bc5fb025f788fce73415486f3c220637420662
Daniel <mills.daniel320@gmail.com>
2026-03-14 12:35:42 -0500

    Prototyping how quiz should work

---
+++ script.js
@@ -0,0 +1,20 @@
+// header with question categories, icon, and light/dark StrictMode
+// question section, with what # question you're on, the question, and a progress bar
+// an answer section, with answer choices and a next question button
+// a completed state, questions are replaced with completed message, answers replaced with score,
+// next button replaced with play again button
+// correct answer styles, hover/active styles, incorrect answer styles, and no answer message
+// main menu, setup like the quiz, but with the categories instead of answers, and no category header, just light/dark
+
+//current plan: create a component for each piece, use js and json data to fill components,
+//  i want a next question transition
+//componenets:
+// header
+// question display
+// current question display
+// progress bar
+// form
+// answer cards
+// button
+// score card
+
