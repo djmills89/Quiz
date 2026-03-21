03abe94f07452ec27923ddf9618932ddac8f4f0c
Daniel <mills.daniel320@gmail.com>
2026-03-21 16:44:56 -0500

    Added design system, updated styling to match system

--- scripts/renderQuiz.js
+++ scripts/renderQuiz.js
@@ -1,17 +1,23 @@
 function renderQuiz(quizData) {
     // data
     let userChoice = 'html'
-    let currentQuestion = 6
+    let currentQuestion = 2
     let currentQuiz = quizData.quizzes.find( quiz => quiz.title.toLowerCase() === userChoice.toLowerCase())
 
+    const titleDisplay = document.querySelector('.title')
+    const questionContainer = document.querySelector('.question__container')
     const questionDisplay = document.querySelector('.question__display')
+    const questionNumberDisplay = document.querySelector('.question__number')
     const answerChoices = document.querySelectorAll('.answer__choices')
     const progressBar = document.querySelector('.progress__bar')
     const quizChoiceButtons = document.querySelectorAll('button')
     quizChoiceButtons.forEach((button) => {
         button.addEventListener('click', (e) => {
             e.preventDefault
-            userChoice = e.target.value
+            titleDisplay.classList.add('hide__content')
+            questionContainer.classList.remove('hide__content')
+            questionNumberDisplay.textContent = `Question ${currentQuestion + 1} of ${currentQuiz.questions.length}`
+            userChoice = e.currentTarget.value
             currentQuiz = quizData.quizzes.find( quiz => quiz.title.toLowerCase() === userChoice.toLowerCase())
             progressBar.style.width = `calc(${currentQuestion + 1} / 10 * 100%)`
 
